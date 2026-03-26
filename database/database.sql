60d80f90a78f7c60b7cce2fb2de68ebd6f53b00a
Sabir Siam <sabirsiam236@gmail.com>
2026-03-26 10:21:59 +0600

    Changed the password size to varchar(255)

--- database/database.sql
+++ database/database.sql
@@ -7,7 +7,7 @@ CREATE TABLE users (
     username VARCHAR(50) UNIQUE NOT NULL,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     profile_picture VARCHAR(500),
     is_banned BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
